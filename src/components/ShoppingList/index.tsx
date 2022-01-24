a8b31ce8b3672f6b1c9fa26168f4102e43582d8f
Victor Manoel <victor.manoel8@hotmail.com>
2022-01-23 21:14:52 -0300

    application with implamentation firebase

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,13 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FlatList } from 'react-native';
+import firestore from '@react-native-firebase/firestore'
 
 import { styles } from './styles';
 import { Product, ProductProps } from '../Product';
 
-import { shoppingListExample } from '../../utils/shopping.list.data';
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection('products')
+      // .where('quantity', '==', 4) // Filtro
+      // .limit(2) // Limitador
+      // .orderBy('quantity') // Ordenação
+      // .startAt(2) //Intervalo
+      // .endAt(4) //Intervalo
+      .onSnapshot(querySnapshot => {
+        const data = querySnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+        }) as ProductProps[]
+
+        setProducts(data)
+      })
+
+
+    return () => subscribe()
+  }, [])
+
+  // Leitura única de um documento
+  // useEffect(() => {
+  //   firestore()
+  //     .collection('products')
+  //     .doc('OLVdY3u87hQyt4nKLRyM')
+  //     .get()
+  //     .then(response => {
+  //       console.log({
+  //         id: response.id,
+  //         ...response.data()
+  //       })
+  //     })
+  //     .catch((error) => console.log(error))
+  // }, [])
 
   return (
     <FlatList
